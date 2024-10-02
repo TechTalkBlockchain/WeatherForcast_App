2bbc8b3a2b53175105f6423c9c961df88f4fa2ce
TechTalkBlockchain <ponfa_m@outlook.com>
2024-10-02 15:57:35 +0100

    Setting APP_API And UsingEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import './App.css'
+import './App.css';
+import axios from 'axios';
 
 
 function App (){
@@ -10,12 +11,42 @@ function App (){
   })
 
   useEffect(()=> {
+    fetchData()
+
+    }, [])
+  
+    const fetchData = async (city) => {
+    try {
+      const APIKEY = process.env.REACT_APP_API_KEY
+
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${'jos'}&appid=${APIKEY}&units=metric`)
+      await setAllData ({
+        city: result.data.name,
+        country:result.data.sys.country,
+        temperature:result.data.main.temp
+      })
+    } catch (e){
+      console.log ('API not Loaded Correctly')
+    }
+  }
     
-  })
+
   return(
+
+    <main>
     <div className="App">
-      {console.log('Testing 123')}
-    </div>)
+      {console.log('1. Testing For: Successful Deployment', 
+        '2. Test for fetching WeatherAPI (Country, City, Weather)',
+         allData.country, allData.city, allData.temperature)}
+    </div>
+    <section>
+      <h1> {allData.city}</h1>
+      <h1>{allData.country}</h1>
+      <h1> Temperature <br/>{allData.temperature}</h1>
+
+    </section>
+    </main>
+    );
 }
 
 export default App;
