862afadea047d93e76b011852d80c0440ffa352f
TechTalkBlockchain <ponfa_m@outlook.com>
2024-10-03 18:34:10 +0100

    Forms and Hooks

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 
 function App (){
+  const [search, setSearch] = useState('')
   const [allData, setAllData] = useState({
     city: '',
     country: '',
@@ -19,7 +20,7 @@ function App (){
     try {
       const APIKEY = process.env.REACT_APP_API_KEY
 
-      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${'jos'}&appid=${APIKEY}&units=metric`)
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`)
       await setAllData ({
         city: result.data.name,
         country:result.data.sys.country,
@@ -29,7 +30,17 @@ function App (){
       console.log ('API not Loaded Correctly')
     }
   }
-    
+
+  const handleSubmit = (event) => {
+    console.log(search)
+    event.preventDefault()
+    fetchData(search)
+  }
+
+  const handleChange = (event) => {
+    setSearch(event.target.value)
+    console.log(search)
+  } 
 
   return(
 
@@ -39,6 +50,17 @@ function App (){
         '2. Test for fetching WeatherAPI (Country, City, Weather)',
          allData.country, allData.city, allData.temperature)}
     </div>
+    <form onSubmit={handleSubmit}>
+      <input
+      type='text'
+      name='city'
+      placeholder='City Name'
+      onChange={handleChange}
+      />
+
+      <button>Search</button>
+
+    </form>
     <section>
       <h1> {allData.city}</h1>
       <h1>{allData.country}</h1>
